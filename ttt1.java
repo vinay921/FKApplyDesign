73147aa1f10b0134225d921d683af5c6beecbbc1
VINAY <gatlavinay97@gmail.com>
2020-01-28 00:17:55 +0530

    THIS IS THE FINAL VERSION 1.0

--- ttt1.java
+++ ttt1.java
@@ -1,33 +1,120 @@
-
 import java.util.*;
-class game
+interface person1
 {
-    public static int  z=1;
-    int[][] bd=new int[3][3];
-    public static boolean comp=false;
-    public boolean completed(int p)
+    boolean type(displayer d,checker m);
+}
+interface checker1
+{
+    void whowins(displayer d);
+
+    boolean completed(int p,displayer d);
+}
+interface displayer1
+{
+    void create();
+    void display();
+}
+class displayer implements displayer1
+{
+
+    int[][] bd;
+    int n;
+    int[] r;
+    int[] c;
+    Scanner s=new Scanner(System.in);
+    public void create()
     {
-        return ((bd[0][0]+bd[0][1]+bd[0][2]==p*3)||(bd[1][0]+bd[1][1]+bd[1][2]==p*3)|| (bd[2][0]+bd[2][1]+bd[2][2]==p*3) || (bd[0][0]+bd[1][0]+bd[2][0]==p*3)|| (bd[0][1]+bd[1][1]+bd[2][1]==p*3)|| (bd[0][2]+bd[1][2]+bd[2][2]==p*3)|| (bd[0][0]+bd[1][1]+bd[2][2]==p*3)|| (bd[2][0]+bd[1][1]+bd[0][2]==p*3));
+        System.out.println("enter the size of square block");
+        n=s.nextInt();
+        bd=new int[n][n];
+        r=new int[n];
+        c=new int[n];
     }
     public void display()
     {
-        for(int i=0;i<3;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<3;j++)
+            for(int j=0;j<n;j++)
                 System.out.print(bd[i][j]+" ");
             System.out.println("");
         }
 
 
     }
-    public void whowins()
+}
+class person implements person1
+{
+    Scanner s=new Scanner(System.in);
+    public boolean type(displayer d,checker m)
     {
-        if(completed(1))
+        int x=s.nextInt();
+        int y=s.nextInt();
+        if(x<0||y<0||x>=d.n||y>=d.n)
+        {
+            System.out.println("not a valid position");
+            return false;
+        }
+        if(d.bd[x][y]!=0)
+        {
+            System.out.println("already filled..chose another position");
+            return false;
+
+        }
+        d.bd[x][y]=m.z;
+        d.r[x]+=m.z;
+        d.c[y]+=m.z;
+        return true;
+
+
+    }
+
+}
+class checker implements checker1
+{
+    int  z=1;
+    boolean comp=false;
+
+    public boolean completed(int p,displayer d)
+    {
+        int k=d.n;
+        for(int i=0;i<k;i++)
+        {
+            if(d.r[i]==p*k)
+                return true;
+        }
+        for(int i=0;i<k;i++)
+        {
+            if(d.c[i]==p*k)
+                return true;
+        }
+        int cx=0;
+        for(int i=0;i<k;i++)
+        {
+            if(d.bd[i][i]==p)
+                cx++;
+        }
+        if(cx==k)
+            return true;
+        cx=0;
+        for(int i=0;i<k;i++)
+        {
+            if(d.bd[i][k-1-i]==p)
+                cx++;
+        }
+        if(cx==k)
+            return true;
+
+        return false;
+    }
+
+    public void whowins(displayer d)
+    {
+        if(completed(1,d))
         {
             System.out.println("A WINS");
             comp=true;
         }
-        else if(completed(-1))
+        else if(completed(-1,d))
         {
             System.out.println("B WINS");
             comp=true;
@@ -41,32 +128,25 @@ class game
 
         }
     }
-    public void type(int x,int y,int q)
+
+}
+
+class compu implements person1
+{
+    Random r=new Random();
+
+    public boolean type(displayer d,checker m)
     {
-        if(x<0||y<0||x>2||y>2)
-        {
-            System.out.println("not a valid position");
-            return;
-        }
-        if(bd[x][y]!=0)
-        {
-            if(q==1)
-            {
-                System.out.println("chose another position");
-                return;
-            }
-            else
-            {
-                return;
-            }
-        }
-        if(z==-1)
-            System.out.println("computer choose "+x+" "+y);
-        bd[x][y]=z;
-        if(z==1)
-            z=-1;
-        else
-            z=1;
+        int x,y,k=d.n;
+        do{
+            x=r.nextInt(k);
+            y=r.nextInt(k);
+        }while(d.bd[x][y]!=0);
+        d.bd[x][y]=-1;
+        d.r[x]+=-1;
+        d.c[y]+=-1;
+        System.out.println("computer choose "+x+" "+y);
+        return true;
     }
 
 }
@@ -74,35 +154,49 @@ public class Main
 {
     public static void main(String args[])
     {
+        System.out.println("WHATS YOUR UPTO BUDDY::");
         System.out.println("1. GAME BETWEEN TWO HUMANS");
         System.out.println("2. GAME BETWEEN COMPUTER");
         Scanner s=new Scanner(System.in);
-        game g=new game();
-        Random r=new Random();
-        int x=0,y=0,q=0;
-        q=s.nextInt();
-        while(!g.comp)
+        int q=s.nextInt();
+        displayer d=new displayer();
+        d.create();
+
+        checker m=new checker();
+
+        person a=new person();
+        person1 b;
+        if(q==1)
         {
-            if(g.z==1)
+            b=new person();
+        }
+        else
+        {
+            b=new compu();
+        }
+        int x,y;
+        while(!m.comp)
+        {
+            if(m.z==1)
+            {
                 System.out.println("persom a turn");
-            else
-                System.out.println("person b turn");
-            if(g.z==1)
-            {
-                x=s.nextInt();
-                y=s.nextInt();
-            }else
-            {
-                x=r.nextInt(3);
-                y=r.nextInt(3);
+
+                if(a.type(d,m))
+                    m.z=-1;
+
             }
-            g.type(x,y,q);
-            g.display();
-            g.whowins();
+            else
+            {
+                System.out.println("person b turn");
+                if(b.type(d,m))
+                    m.z=1;
+
+            }
+            d.display();
+            m.whowins(d);
+
 
         }
-
-
 
     }
 }
